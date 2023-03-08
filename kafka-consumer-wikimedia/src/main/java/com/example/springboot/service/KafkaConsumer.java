0d8f45ad31d788e1d0e4797d8b454b486a4645d7
BKD471 <bhaskarkumardas9@gmail.com>
2023-03-08 21:26:55 +0530

    Storing the event data to db

--- kafka-consumer-wikimedia/src/main/java/com/example/springboot/service/KafkaConsumer.java
+++ kafka-consumer-wikimedia/src/main/java/com/example/springboot/service/KafkaConsumer.java
@@ -1,5 +1,7 @@
 package com.example.springboot.service;
 
+import com.example.springboot.entity.WikimediaData;
+import com.example.springboot.repository.WikimediaDataRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
@@ -7,12 +9,18 @@ import org.springframework.stereotype.Service;
 @Service
 @Slf4j
 public class KafkaConsumer {
+    private WikimediaDataRepository dataRepository;
+    KafkaConsumer(WikimediaDataRepository dataRepository){
+        this.dataRepository=dataRepository;
+    }
+
     @KafkaListener(topics = "${spring.kafka.topic.name}",
             groupId = "${spring.kafka.consumer.group-id}")
-   public void consumeMessage(String eventMessage){
-        log.info(String.format("Event msg is -> %s",eventMessage));
+    public void consumeMessage(String eventMessage) {
+        log.info(String.format("Event msg is -> %s", eventMessage));
 
-
-   }
-
+        WikimediaData bigEventData=new WikimediaData();
+        bigEventData.setWikiEventData(eventMessage);
+        dataRepository.save(bigEventData);
+    }
 }
